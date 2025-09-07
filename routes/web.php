1f6bc0f0153ac2a2895e57bc678e292684998b14
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2025-09-07 22:18:59 +0700

    chore(route): redirect root and login routes to filament panel #2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('login');
 });
+
+Route::get('/login', function () {
+    return redirect()->route('filament.app.auth.login');
+})->name('login');
