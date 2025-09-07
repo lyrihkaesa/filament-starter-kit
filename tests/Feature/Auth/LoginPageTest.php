2d19cedc2a1d4443c4f862971dac943948dee150
Kaesa Lyrih <50957602+lyrihkaesa@users.noreply.github.com>
2025-09-07 23:40:53 +0700

    test(auth): add feature test for custom Filament login page with autofill #4

---
+++ tests/Feature/Auth/LoginPageTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Filament\Pages\Auth\Login;
+use Illuminate\Support\Facades\Config;
+use Livewire\Livewire;
+
+it('filament login page use custom page', function () {
+    $this->get('/app/login')
+        ->assertSeeLivewire(Login::class);
+});
+
+it('autofills login form when debug mode enabled', function () {
+    // Aktifkan debug mode
+    Config::set('app.debug', true);
+
+    // Pastikan autofill email/password
+    Livewire::test(Login::class)
+        ->assertOk()
+        ->assertSet('data.email', 'admin@example.com')
+        ->assertSet('data.password', 'password')
+        ->assertSet('data.remember', true);
+});
+
+it('does not autofill login form when debug mode disabled', function () {
+    // Matikan debug mode
+    Config::set('app.debug', false);
+
+    // Pastikan autofill email/password
+    Livewire::test(Login::class)
+        ->assertOk()
+        ->assertNotSet('data.email', 'admin@example.com')
+        ->assertNotSet('data.password', 'password')
+        ->assertSet('data.remember', false);
+});
