c76fe88f632672ffeeb08e6046838487e26e345e
Kaesa Lyrih <kaesalyrih@gmail.com>
2026-03-25 23:30:16 +0700

    feat: add laravel permission policy

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -4,35 +4,67 @@ declare(strict_types=1);
 
 namespace App\Policies;
 
-use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
+use Spatie\Permission\Models\Role;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
-// @codeCoverageIgnoreStart
-//
-final class RolePolicy
+class RolePolicy
 {
-    public function viewAny(User $user): bool
+    use HandlesAuthorization;
+    
+    public function viewAny(AuthUser $authUser): bool
     {
-        return $user->hasRole('super_admin');
+        return $authUser->can('ViewAny:Role');
     }
 
-    public function view(User $user): bool
+    public function view(AuthUser $authUser, Role $role): bool
     {
-        return $user->hasRole('super_admin');
+        return $authUser->can('View:Role');
     }
 
-    public function create(User $user): bool
+    public function create(AuthUser $authUser): bool
     {
-        return $user->hasRole('super_admin');
+        return $authUser->can('Create:Role');
     }
 
-    public function update(User $user): bool
+    public function update(AuthUser $authUser, Role $role): bool
     {
-        return $user->hasRole('super_admin');
+        return $authUser->can('Update:Role');
     }
 
-    public function delete(User $user): bool
+    public function delete(AuthUser $authUser, Role $role): bool
     {
-        return $user->hasRole('super_admin');
+        return $authUser->can('Delete:Role');
     }
+
+    public function restore(AuthUser $authUser, Role $role): bool
+    {
+        return $authUser->can('Restore:Role');
+    }
+
+    public function forceDelete(AuthUser $authUser, Role $role): bool
+    {
+        return $authUser->can('ForceDelete:Role');
+    }
+
+    public function forceDeleteAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('ForceDeleteAny:Role');
+    }
+
+    public function restoreAny(AuthUser $authUser): bool
+    {
+        return $authUser->can('RestoreAny:Role');
+    }
+
+    public function replicate(AuthUser $authUser, Role $role): bool
+    {
+        return $authUser->can('Replicate:Role');
+    }
+
+    public function reorder(AuthUser $authUser): bool
+    {
+        return $authUser->can('Reorder:Role');
+    }
+
 }
-// @codeCoverageIgnoreEnd
