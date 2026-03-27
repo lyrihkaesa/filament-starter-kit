87e40bb273f87880a630b34c3466533b78307c7b
Kaesa Lyrih <kaesalyrih@gmail.com>
2026-03-28 05:20:54 +0700

    users: change icon navigation

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -24,7 +24,7 @@ final class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Users;
 
     public static function form(Schema $schema): Schema
     {
