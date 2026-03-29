1c723feb244da033d959c418725cab08ea9a04b6
Kaesa Lyrih <kaesalyrih@gmail.com>
2026-03-29 22:20:43 +0700

    docs: user deletion and anonym

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+            $table->uuid('deleted_by')->nullable();
             $table->datetime('anonymized_at')->nullable();
         });
 
