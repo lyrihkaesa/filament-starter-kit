e3a41b0e531804d20748651ef3d60ed976e2e2aa
Kaesa Lyrih <kaesalyrih@gmail.com>
2026-04-20 01:43:00 +0700

    feat: initialize application bootstrap configuration and add image driver settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->authenticateSessions();
         $middleware->throttleApi();
         $middleware->alias([
