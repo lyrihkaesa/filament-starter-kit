65d514152f0ab0bc55b380ce09fbd8711df9f227
Kaesa Lyrih <kaesalyrih@gmail.com>
2026-09-11 16:58:54 +0700

    chore: remove axios dependency and initialize bootstrap.js for frontend configuration

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
-window.axios = axios;
+/**
+ * Configure any frontend libraries or bootstrap code here.
+ */
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
